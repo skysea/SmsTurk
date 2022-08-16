dcc2670ec8a2fcb23d28da4e248d9528a55251e8
serkan.akalan <srknkln@gmail.com>
2022-08-16 16:52:05 +0300

    16.08.2022 16:51

--- resources/views/home/theme1/pages/login/login.blade.php
+++ resources/views/home/theme1/pages/login/login.blade.php
@@ -17,7 +17,8 @@
 
     <section class="blog-grid">
         <div class="container">
-
+            <div class="row">
+            <div class="col-md-6">
             <div class="footer-widget footer-widget__newsletter">
                 <h3 class="footer-widget__title">Giriş Yap</h3>
                 <form action="#" class="login-widget__newsletter-form" method="post">
@@ -28,12 +29,19 @@
                         <input class="form-control" type="password" placeholder="Şifrenizi yazınız" name="sifre">
                     </div>
                     <div class="form-group mb-3">
+                        <input class="form-control" type="password" placeholder="Şifrenizi yazınız" name="sifre">
+                    </div>
+                    <div class="form-group mb-3">
 
                         <button type="submit" class="thm-btn cta-one__btn">Giriş Yap</button>
                     </div>
 
                 </form>
-
+            </div>
+            </div>
+            <div class="col-md-6">
+                <img class="img-fluid" src="https://demo.smsonayscripti.com/assets/theme-1/img/photos/tm3.jpg">
+            </div>
             </div>
 
         </div><!-- /.container -->
